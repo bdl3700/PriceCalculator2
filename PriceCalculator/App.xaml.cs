a0873b4702a03e9b2925f8c1c418683eceb57301
bdl3700-desktop <bdl3700@comcast.net>
2022-11-16 23:06:22 -0700

    Updated to .Net 7

--- PriceCalculator/App.xaml.cs
+++ PriceCalculator/App.xaml.cs
@@ -1,11 +1,26 @@
-﻿namespace PriceCalculator;
+﻿using System.Diagnostics;
 
-public partial class App : Application
+namespace PriceCalculator
 {
-	public App()
-	{
-		InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var w = base.CreateWindow(activationState);
+
+            // default values that will later be ovewritten by a fraction of the display's dimensions
+            w.MinimumWidth = 300;
+            w.MaximumWidth = 300;
+            w.MinimumHeight =600;
+            w.MaximumHeight = 600;
+            return w;
+        }
+    }
 }
--- PriceCalculator/AppShell.xaml.cs
+++ PriceCalculator/AppShell.xaml.cs
@@ -1,9 +1,10 @@
-﻿namespace PriceCalculator;
-
-public partial class AppShell : Shell
+﻿namespace PriceCalculator
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
 }
